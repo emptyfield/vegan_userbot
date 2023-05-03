57664c3a014ccc823c9874a33b65ccac8bd25450
Почтаренко Арсений <alpinfor100000@gmail.com>
2023-05-03 10:35:40 +0300

    Add items constant

--- constants.py
+++ constants.py
@@ -11,6 +11,9 @@ battle_targets = {
 skills = {'ninja', 'inzhener', 'alchemist', 'zombie', 'medic', 'vor', 'protivogaz', 'berserk',
           'sadist', 'incvizitor', 'shieldgen', 'greedy', 'bicepc', 'goodsleep', 'cherep', 'pyrotech', 'resistant', 'dvuzhil', 'zapas', 'armor'}
 
+items = {'dzet', 'adrenaline', 'hitin', 'flash',
+         'shield', 'grenade', 'molotov', 'knife'}
+
 battle_time = [6, 14, 22]
 
 vegan_id = 5505670334
