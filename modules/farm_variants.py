2286a6d303954b99b12c8306caa35e3a8ccd352b
Почтаренко Арсений <alpinfor100000@gmail.com>
2023-05-06 23:57:19 +0300

    Rename patrol_callback

--- modules/farm_variants.py
+++ modules/farm_variants.py
@@ -8,7 +8,7 @@ from utils.filters import f_vegan
 from constants import vegan_id, melee_weapon_text, time_for_regen
 
 
-async def patrol_callback(c: Client, *args):
+async def inline_callback(c: Client, *args):
     try:
         await c.request_callback_answer(*args)
     except:
@@ -28,10 +28,10 @@ async def patrol(c: Client, m: types.Message, user):
     args = (vegan_id, quest_message_id,
             "quest_select?castle_protect", 1)
 
-    user.scheduler.add_job(patrol_callback, 'interval',
+    user.scheduler.add_job(inline_callback, 'interval',
                            (c, args), id=f'{c.name}/farm', seconds=182)
 
-    await patrol_callback(c, *args)
+    await inline_callback(c, *args)
 
     if m != None:
         await m.edit(res_header(c.name, m.text) + 'Автопатруль запущен')
@@ -51,10 +51,10 @@ async def rathunt(c: Client, m: types.Message, user):
 
     async def result_handler(c: Client, m: types.Message):
         if m.text.startswith('Вы одержали победу над крысой!'):
-            await patrol_callback(c, *args)
+            await inline_callback(c, *args)
         else:
             await sleep(time_for_regen * 2)
-            await patrol_callback(c, *args)
+            await inline_callback(c, *args)
 
     result = c.add_handler(MessageHandler(result_handler, f_vegan & regex(
         r'^(Вы одержали победу над крысой!|Вы не смогли одолеть крысу, и вернулись в замок залечивать раны.)\n')))
@@ -93,6 +93,6 @@ async def rathunt(c: Client, m: types.Message, user):
     handlers.extend([attackselect, result, turn, weaponselect])
     user.state['rathunt_handlers'] = handlers
 
-    await patrol_callback(c, *args)
+    await inline_callback(c, *args)
     if m != None:
         await m.edit(res_header(c.name, m.text) + 'Автоохота на крыс запущена')
